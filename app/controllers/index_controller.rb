b8ba919578ba61afc019343ea76e63479d87ab7a
mkolonas <claudia.kolonas@gmail.com>
2015-04-06 01:08:26 -0400

    form added

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,16 +1,3 @@
 class IndexController < ApplicationController
 
-	def new
-		@user = User.new
-	end 
-
-	def create
-		@user = User.new(user_params)
-		@user.save #Saves tweet to database
-	end
-
-	def user_params
-		params.require(:name).permit(:email,:message)
-	end
-
 end
